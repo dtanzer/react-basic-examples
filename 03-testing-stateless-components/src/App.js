53aada8ee7909e71f0e99cc7d26a84e369353d71
David Tanzer <business@davidtanzer.net>
2017-03-06 11:55:06 +0100

    Added code that should be tested

--- 03-testing-stateless-components/src/App.js
+++ 03-testing-stateless-components/src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import User from './User';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ul>
+        <User firstName="John" lastName="Doe" email="john.doe@example.com" active={true} />
+        <User firstName="Jane" lastName="Doe" />
+      </ul>
     );
   }
 }
