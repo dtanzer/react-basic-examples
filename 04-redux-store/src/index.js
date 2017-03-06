fcb021f528d8938a3d0cdf55b34b856a4afa69eb
David Tanzer <business@davidtanzer.net>
2017-03-06 14:33:01 +0100

    Added initial state to be rendered to redux example

--- 04-redux-store/src/index.js
+++ 04-redux-store/src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const initialState = {
+  users: [
+    { firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com', active: 'true'},
+    { firstName: 'Jane', lastName: 'Doe' },
+    { firstName: 'Mike', lastName: 'Nobody' },
+    { firstName: 'Mary', lastName: 'Sims' },
+  ]
+};
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
