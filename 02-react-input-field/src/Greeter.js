7b19118bd0f2b0706902e7499b9f601bb66862b5
David Tanzer <business@davidtanzer.net>
2017-03-06 11:34:54 +0100

    Finished second example

--- 02-react-input-field/src/Greeter.js
+++ 02-react-input-field/src/Greeter.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 class Greeter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      toGreet: "",
+      greeting: ""
+    };
+  }
+
   render() {
     return (
-      <div>Hello {this.props.name}</div>
+      <div>
+        <input type="text" value={this.state.toGreet} onChange={ e => this.setGreeting(e.target.value) }/>
+        <p>{this.state.greeting}</p>
+      </div>
     );
+  }
+
+  setGreeting(toGreet) {
+    const greeting = toGreet ? "Hello " + toGreet : "";
+    this.setState({
+      toGreet,
+      greeting
+    });
   }
 }
 
