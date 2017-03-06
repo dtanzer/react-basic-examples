bf036cd7195bdaa4dfc5fd491244d908cab25c8d
David Tanzer <business@davidtanzer.net>
2017-03-06 11:05:54 +0100

    Added basic code for second example: stateful input field

---
+++ 02-react-input-field/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Greeter from './Greeter'
+
+class App extends Component {
+  render() {
+    return (
+      <Greeter name="World" />
+    );
+  }
+}
+
+export default App;
