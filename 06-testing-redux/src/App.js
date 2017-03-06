38022bae0258326921a029a8ed8872ae0103d7e9
David Tanzer <business@davidtanzer.net>
2017-03-06 16:05:28 +0100

    Added example for testing redux

---
+++ 06-testing-redux/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { GreeterContainer } from './Greeter'
+
+class App extends Component {
+  render() {
+    return (
+      <GreeterContainer />
+    );
+  }
+}
+
+export default App;
---
+++ 06-testing-redux/src/GreeterActions.js
@@ -0,0 +1,3 @@
+export default {
+  greetingChanged: Symbol("GREETING_CHANGED")
+};
