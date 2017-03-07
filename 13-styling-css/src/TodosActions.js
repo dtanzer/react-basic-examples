78d39b7cc32853b93a59387990a7e7bf1909298a
David Tanzer <business@davidtanzer.net>
2017-03-07 10:03:16 +0100

    Merged starting point for styling/css example from sample-solution

---
+++ 13-styling-css/src/TodosActions.js
@@ -0,0 +1,6 @@
+export default {
+  newTodoCreated: Symbol("NEW_TODO_CREATED"),
+  newTodoTextChanged: Symbol("NEW_TODO_TEXT_CHANGED"),
+  todoStateToggled: Symbol("TODO_STATE_TOGGLED"),
+  tabSwitched: Symbol("TAB_SWITCHED")
+};
