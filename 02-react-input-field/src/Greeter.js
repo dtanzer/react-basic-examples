04c25f808948419a49a66a8cd0e614d4b11f064b
David Tanzer <business@davidtanzer.net>
2017-03-13 07:10:02 +0100

    Lab-02

--- 02-react-input-field/src/Greeter.js
+++ 02-react-input-field/src/Greeter.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 class Greeter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toGreet: "World!"
+    };
+  }
   render() {
+    const greeting = this.state.toGreet ? <p>Hello {this.state.toGreet}</p> : <p></p>;
     return (
-      <div>Hello {this.props.name}</div>
+      <div>
+        <input type="text" value={this.state.toGreet} onChange={e => this.setState({toGreet: e.target.value})} />
+        {greeting}
+      </div>
     );
   }
 }
